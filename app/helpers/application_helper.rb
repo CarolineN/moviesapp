1b973574172a1104caf30b7b2688d22664a4e048
Caroline Nolan <caroline.nolan2@student.dit.ie>
2015-01-26 15:52:00 +0000

    New added login/layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def signed_in?
+		if session[:user_id].nil?
+			return
+		else
+			@current_user = User.find_by_id(session[:user_id])
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Moviesapp::Application.routes.draw do
+ get "pages/home"
+ 
+ get "pages/about"
+ 
+ get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
   resources :genres
 
   resources :movies
+  
+  resources :sessions
+  match '/signin', :to =>'sessions#new'
+  match '/signout', :to =>'sessions#destroy'
+  match '/about', :to => 'Pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +69,7 @@ Moviesapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
